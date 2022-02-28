9ac4904111c3bc219c7434fc06043cf07994c818
ptgamerty <97254606+ptgamerty@users.noreply.github.com>
2022-02-28 19:42:11 +0000

    Add files via upload

---
+++ js/Cannon.js
@@ -0,0 +1,23 @@
+class Cannon {
+  constructor(x, y, width, height, angle) {
+    this.x = x;
+    this.y = y;
+    this.width = width;
+    this.height = height;
+    this.angle = angle;
+    this.cannonImage = loadImage("assets/canon.png")
+    this.cannonBase = loadImage("assets/cannonBase.png")
+  }
+  display(){
+    push()
+    translate(this.x,this.y)
+    rotate(this.angle)
+imageMode(CENTER)
+image(this.cannonImage,0,0,this.width,this.height)
+pop()
+image(this.cannonBase,70,20,200,200)
+noFill()
+
+  }
+  
+}
